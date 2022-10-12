492e9368dc77dcb4d7d3f34130d02013a23b3a60
kanno-takumi <bp20065@shibaura-it.ac.jp>
2022-10-13 00:56:16 +0900

    make add_method

--- todolist_db/src/main/java/com/example/todolist/HomeController.java
+++ todolist_db/src/main/java/com/example/todolist/HomeController.java
@@ -1,6 +1,9 @@
 package com.example.todolist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +16,29 @@ import java.util.UUID;
 @Controller
 public class HomeController {
     private List<TaskItem> taskitems=new ArrayList<>();
+    private TaskListDao dao;
+
+    @Autowired
+    HomeController(TaskListDao dao){
+        this.dao=dao;
+    }
 
     @GetMapping(value="/list")//urlで/listが呼ばれたとき
     String listItems(Model model){
-        model.addAttribute("taskList",taskitems);
+      //  model.addAttribute("taskList",taskitems);//TaskItemのリストを渡している→TaskListDaoでTaskItemのオブジェクトを作成すればOK
         return "home";
     }
+
     @GetMapping("/add")
     String addItem(@RequestParam("task") String task,@RequestParam("deadline") String deadline){
         String id=UUID.randomUUID().toString().substring(0,8);
         TaskItem item=new TaskItem(id,task,deadline,false);
-        taskitems.add(item);
+        //編集
+        //taskitems.add(item);
+        //追加
+
+        dao.add(item);
+
         return "redirect:/list";
     }
     @GetMapping("/delete")
